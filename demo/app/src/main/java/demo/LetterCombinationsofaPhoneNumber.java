0686906d3330924c0bf4ff7e269c6093277a49c9
erenoa123 <brack.lost.barn@gmail.com>
2021-12-04 16:36:22 +0900

    format enable

--- demo/app/src/main/java/demo/LetterCombinationsofaPhoneNumber.java
+++ demo/app/src/main/java/demo/LetterCombinationsofaPhoneNumber.java
@@ -6,28 +6,29 @@ import java.util.List;
 
 public class LetterCombinationsofaPhoneNumber {
     public List<String> letterCombinations(String digits) {
-        
-        //　前処理
+
+        // 前処理
         // phonenumberに対応する文字列リストを作成
         // 入力値を配列へ変換
         String[][] strAry = setUp();
         String[] digitsAry = digits.split("");
 
-        //　入力値が空の場合、空のリストを返却
+        // 入力値が空の場合、空のリストを返却
         // 入力値が1桁の場合、組み合わせ処理を実施せずリストを作成して返却
-        if(digits.isEmpty()){
+        if (digits.isEmpty()) {
             return new ArrayList<>();
-        }else if(digits.length() == 1){
+        } else if (digits.length() == 1) {
             return Arrays.asList(strAry[Integer.parseInt(digitsAry[0])]);
         }
 
         // 返却値リストを作成
-        List<String> retList = combination(Arrays.asList(strAry[Integer.parseInt(digitsAry[0])]), strAry[Integer.parseInt(digitsAry[1])]);
+        List<String> retList = combination(Arrays.asList(strAry[Integer.parseInt(digitsAry[0])]),
+                strAry[Integer.parseInt(digitsAry[1])]);
 
         // 入力値が2桁以上の場合は、返却値リストとの組み合わせをさらに実施
-        for(int i = 2 ; i < digits.length() ; i++){
+        for (int i = 2; i < digits.length(); i++) {
             retList = combination(retList, strAry[Integer.parseInt(digitsAry[i])]);
-            
+
         }
 
         return retList;
@@ -38,15 +39,15 @@ public class LetterCombinationsofaPhoneNumber {
      * 
      * @param parentList
      * @param childList
-     * @return　組み合わせ結果
+     * @return 組み合わせ結果
      */
-    private List<String> combination(List<String> parentList, String[] childList){
+    private List<String> combination(List<String> parentList, String[] childList) {
 
         List<String> retList = new ArrayList<>();
 
-        for(String s : parentList){
-            for (String chi: childList){
-                retList.add(s+chi);
+        for (String s : parentList) {
+            for (String chi : childList) {
+                retList.add(s + chi);
             }
         }
         return retList;
@@ -58,21 +59,21 @@ public class LetterCombinationsofaPhoneNumber {
      * 
      * @return
      */
-    private String[][] setUp(){
+    private String[][] setUp() {
 
         String[][] strarr = {
-            {},
-            {},
-            {"a","b","c"},
-            {"d","e","f"},
-            {"g","h","i"},
-            {"j","k","l"},
-            {"m","n","o"},
-            {"p","q","r","s"},
-            {"t","u","v"},
-            {"w","y","x","z"}
+                {},
+                {},
+                { "a", "b", "c" },
+                { "d", "e", "f" },
+                { "g", "h", "i" },
+                { "j", "k", "l" },
+                { "m", "n", "o" },
+                { "p", "q", "r", "s" },
+                { "t", "u", "v" },
+                { "w", "y", "x", "z" }
         };
-        
+
         return strarr;
 
     }
--- demo/app/src/test/java/demo/ThreeSumClosestSolutionTest.java
+++ demo/app/src/test/java/demo/ThreeSumClosestSolutionTest.java
@@ -2,8 +2,6 @@ package demo;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 public class ThreeSumClosestSolutionTest {
